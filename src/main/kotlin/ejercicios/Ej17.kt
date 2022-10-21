93dca90a25b2d5a2753109cb67a5e04b50db019a
JorgeYu <jorgeyu566@gmail.com>
2022-10-21 10:01:51 +0200

    17 end

--- src/main/kotlin/ejercicios/Ej17.kt
+++ src/main/kotlin/ejercicios/Ej17.kt
@@ -2,8 +2,38 @@ package ejercicios
 
 fun main() {
 
-    println("Introduce la cadena de caracteres: ")
-    var cadenas = readln()
+    val voca:Array<Char> = arrayOf('a', 'e', 'i', 'o', 'u')
+
+    var contVoca = 0
+    var contnum = 0
+    var contconso = 0
+    var contspc = 0
 
 
-}
+
+
+    print("Introduce la cadena de caracteres: ")
+    val cadenas = readln()
+
+    for(i in cadenas){
+        if(i in voca){
+            contVoca++
+        } else if(i.digitToIntOrNull() is Int){
+            contnum++
+        }else if(i == ' '){
+            contspc++
+        }
+        else{
+            contconso++
+        }
+    }
+
+    println("hay $contVoca vocales")
+    println("hay $contconso consonantes")
+    println("hay $contnum numeros")
+    println("hay $contspc espacios")
+
+
+
+
+}//fin del main
