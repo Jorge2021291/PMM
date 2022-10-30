f086f97adc79ee854cf5418bd16f94020972fc82
ELKRAZY <elkrazyking@gmail.com>
2022-10-30 21:14:31 +0100

    21 done

---
+++ src/main/kotlin/ejercicios/Ej21.kt
@@ -0,0 +1,22 @@
+package ejercicios
+
+fun main() {
+    print("introduce el la frase: ")
+    val frase = readln()
+    var fraseFinal = ""
+
+    for(i in frase){
+        if(i != ' '){
+            fraseFinal += i
+
+
+        }else if(i == ' '){
+        }
+    }
+    print(fraseFinal)
+
+
+
+
+
+}
